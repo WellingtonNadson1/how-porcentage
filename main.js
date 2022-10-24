0871c63510a0599c0a512513b898dfaeade81ba5
WellingtonNadson1 <wellingtonnadson@gmail.com>
2022-10-24 12:48:50 -0300

    Tratamet excecion

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 import './style.css';
 
 const totalPorcento = document.querySelector('[data-js="totalPorcento"]');
-const meuPorcento = document.querySelector('[data-js="meuPorcento"]');
+let meuPorcento = document.querySelector('[data-js="meuPorcento"]');
 const result = document.querySelector('[data-js="result"]');
 const form = document.querySelector('#form');
 const btnReset = document.querySelector('[data-js="btnReset"]');
 
 form.addEventListener('submit', (e) =>{
   e.preventDefault();
-  const resultPorcentos = (Number(meuPorcento.value) * 100) / Number(totalPorcento.value)
-
-  result.value = resultPorcentos.toFixed(2)
+    const resultPorcentos = (Number(meuPorcento.value) * 100) / Number(totalPorcento.value)
+    result.value = resultPorcentos.toFixed(2)
+  if (!resultPorcentos) {
+    alert('Preencha os campos')
+    result.value = ''
+  }
 })
 
 btnReset.addEventListener('click', (e) => {
