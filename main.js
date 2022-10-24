42b64e81aae9c0fcf17e57d55c88af151543a745
WellingtonNadson1 <wellingtonnadson@gmail.com>
2022-10-24 09:35:22 -0300

    Added button of reset

--- main.js
+++ main.js
@@ -4,10 +4,18 @@ const totalPorcento = document.querySelector('[data-js="totalPorcento"]');
 const meuPorcento = document.querySelector('[data-js="meuPorcento"]');
 const result = document.querySelector('[data-js="result"]');
 const form = document.querySelector('#form');
+const btnReset = document.querySelector('[data-js="btnReset"]');
 
 form.addEventListener('submit', (e) =>{
   e.preventDefault();
   const resultPorcentos = (Number(meuPorcento.value) * 100) / Number(totalPorcento.value)
 
   result.value = resultPorcentos.toFixed(2)
+})
+
+btnReset.addEventListener('click', (e) => {
+  e.preventDefault();
+  totalPorcento.value = '';
+  meuPorcento.value = ''
+  result.value = ''
 })
