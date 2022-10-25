d509100d06d901904bb319365d5360e2844e2a14
WellingtonNadson1 <wellingtonnadson@gmail.com>
2022-10-25 09:27:55 -0300

    Added modal

--- main.js
+++ main.js
@@ -5,16 +5,24 @@ let meuPorcento = document.querySelector('[data-js="meuPorcento"]');
 const result = document.querySelector('[data-js="result"]');
 const form = document.querySelector('#form');
 const btnReset = document.querySelector('[data-js="btnReset"]');
-
+const modal = document.querySelector('[data-js="modal"]');
 form.addEventListener('submit', (e) =>{
   e.preventDefault();
     const resultPorcentos = (Number(meuPorcento.value) * 100) / Number(totalPorcento.value)
     result.value = resultPorcentos.toFixed(2)
   if (!resultPorcentos) {
-    alert('Preencha os campos')
+    modal.classList.remove("hidden")
+    // alert('Preencha os campos')
     result.value = ''
   }
 })
+
+window.addEventListener('click',function(event){
+  if(event.target != modal && event.target.parentNode != modal){
+    modal.classList.add("hidden")
+  }
+});
+
 
 btnReset.addEventListener('click', (e) => {
   e.preventDefault();
